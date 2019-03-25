6b124130a0b79af8b1c6a3f4b4ba772bdd2f61dc
Xavier Devroey <xavier.devroey@gmail.com>
2019-03-25 18:01:36 +0100

    Adding function to generate reproduction graph for the crashes

--- results/Rscripts/rq1.r
+++ results/Rscripts/rq1.r
@@ -3,7 +3,7 @@
 # author: Xavier Devroey
 
 library(ggplot2)
-library(dplyr)
+library(tidyverse)
 
 source('dataclean.r')
 
@@ -73,10 +73,10 @@ plotSummary <- function(majority){
 
 plotSummaryPieChart <- function(majority){
   df <- majority %>%
-  group_by(majority_result_factor) %>%
-  summarise(n = n()) %>%
-  mutate(Frequency = n / sum(n), label = paste0(n)) %>%
-  mutate(Tool = "EvoCrash_results")
+    group_by(majority_result_factor) %>%
+    summarise(n = n()) %>%
+    mutate(Frequency = n / sum(n), label = paste0(n)) %>%
+    mutate(Tool = "EvoCrash_results")
   p <- ggplot(df, aes(x = Tool, y = Frequency, fill = majority_result_factor)) + 
    geom_bar(stat = "identity", width = 1) +
     geom_text(aes(label = label), position = position_stack(vjust = 0.5), size = 5) +
@@ -92,6 +92,56 @@ plotSummaryPieChart <- function(majority){
           ) +
     coord_polar("y", start=0)
 }
+
+STATUS_LEVELS = c("not reproduced", "reproduced")
+
+getReproduceStatus <- function(results){
+  df <- results %>%
+    group_by(case, majority_result) %>%
+    mutate(max_reproduced = ifelse(majority_result == 'reproduced', max(frame_level), 0)) %>%
+    ungroup() %>%
+    group_by(case) %>%
+    mutate(max_reproduced = max(max_reproduced)) %>%
+    ungroup() %>%
+    distinct(application_name, application_factor, case, exception, exception_factor, max_reproduced)
+  df <- data.frame(df) %>%
+    group_by(case) %>%
+    mutate(highest = max(max_reproduced)) %>%
+    ungroup() %>%
+    mutate(status = ifelse(max_reproduced > 0 & max_reproduced >= highest, STATUS_LEVELS[2], STATUS_LEVELS[1]))
+  df <- data.frame(df)
+  df$status_factor <- factor(df$status, levels = STATUS_LEVELS, ordered = TRUE)
+  return(df)
+}
+
+
+buildStackedBarCrashStatusPerApp <- function(majority){
+  # Get result status
+  reproduction <- getReproduceStatus(majority) 
+  # Add count and frequency 
+  df <- reproduction %>%
+    mutate(application_name = '(all)')
+  df$application_factor = factor(df$application_name, levels = c(levels(df$application_factor), '(all)'))
+  df <- df %>%
+    rbind(reproduction) %>%
+    group_by(application_factor, exception_factor, status_factor) %>%
+    summarise(n = n()) %>%
+    mutate(Frequency = n / sum(n), label = paste0(n))
+  df$exception_factor <- fct_rev(df$exception_factor)
+  p <- ggplot(df, aes(x = exception_factor, y = Frequency, fill = status_factor)) + 
+    geom_bar(stat = "identity") +
+    geom_text(aes(label = label), position = position_stack(vjust = 0.5), size = 3) +
+    scale_fill_brewer(palette=colorpalette) +
+    scale_y_continuous(labels = scales::percent) +
+    xlab(NULL) +
+    ylab(NULL) +
+    guides(fill=guide_legend(title=NULL)) +
+    theme(legend.position="bottom") +
+    coord_flip() +
+    facet_wrap(. ~ application_factor, ncol = 4)
+  return(p)
+}
+
 # ------------------------------
 # Main function definition
 # ------------------------------
@@ -108,11 +158,12 @@ main <- function(){
 	p <- plotGroupedCoverageRangesAllApps(frequent)
 	ggsave(plot = p, filename = '../plots/rq1_all.pdf', width=230, height=250, units = "mm" )
 	
-	p <- plotSummary(frequent)
-	ggsave(plot = p, filename = '../plots/rq1_summary.pdf', width=140, height=80, units = "mm" )
+	#p <- plotSummary(frequent)
+	#ggsave(plot = p, filename = '../plots/rq1_summary.pdf', width=140, height=80, units = "mm" )
 	
 	p <- plotSummaryPieChart(frequent)
-	ggsave(plot = p, filename = 'rq1_summary_pieChart.pdf', width=130, height=130, units = "mm" )
+	ggsave(plot = p, filename = '../plots/rq1_summary.pdf', width=130, height=130, units = "mm" )
+	
 	# Compute values from the text
 	
 	cat("Most frequent outcome frequencies:", "\n")
